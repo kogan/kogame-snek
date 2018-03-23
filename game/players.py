774a37f3191ca04285390617309bc726613e5e9e
Josh Smeaton <josh.smeaton@gmail.com>
2018-03-23 13:22:12 +1100

    Manage player state

---
+++ game/players.py
@@ -0,0 +1,31 @@
+from enum import Enum, unique
+from typing import Mapping
+
+from django.core.cache import cache
+from redis.client import StrictRedis
+from .models import Game
+
+
+@unique
+class Direction(Enum):
+    UP = (0, -1)
+    DOWN = (0, 1)
+    LEFT = (-1, 0)
+    RIGHT = (1, 0)
+
+
+def set_player_direction(game: Game, player: str, direction: Direction):
+    redis: StrictRedis = cache.client.get_client()
+    hash_key = f'playerdirections.{game.pk}'
+    return redis.hset(hash_key, player, direction.name)
+
+
+def get_player_directions(game) -> Mapping[str, Direction]:
+    redis: StrictRedis = cache.client.get_client()
+    hash_key = f'playerdirections.{game.pk}'
+    directions: Mapping[bytes, bytes] = redis.hgetall(hash_key)
+    return {
+        player.decode('utf8'): Direction[direction.decode('utf8')]
+        for player, direction in directions.items()
+    }
+    return directions
