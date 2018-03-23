73adfae2eb3eb4f8f6a182c93903240730a307d1
Jake Barber <jake.b.dev@gmail.com>
2018-03-23 15:29:07 +1100

    add session middleware, import recevie consumer endpoint

--- kogame/routing.py
+++ kogame/routing.py
@@ -1,14 +1,17 @@
 from channels.routing import ChannelNameRouter, ProtocolTypeRouter, URLRouter
+from channels.sessions import SessionMiddlewareStack
 from django.conf.urls import url
 from game.consumers import GameConsumer, PlayerConsumer
 
 from chat.consumers import ChatConsumer
 
 application = ProtocolTypeRouter({
-    'websocket': URLRouter([
-        url(r'^game/$', PlayerConsumer),
-        url(r'^ws/chat/(?P<room_name>[^/]+)/$', ChatConsumer),
-    ]),
+    "websocket": SessionMiddlewareStack(
+        URLRouter([
+            url(r'^game/$', PlayerConsumer),
+            url(r'^ws/chat/(?P<room_name>[^/]+)/$', ChatConsumer),
+        ])
+    ),
     'channel': ChannelNameRouter({
         'game_engine': GameConsumer,
     }),
