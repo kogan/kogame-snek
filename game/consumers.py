6c92e50ec89ae02cef2b9691d3acf6b42f1c6017
Josh Smeaton <josh.smeaton@gmail.com>
2018-03-23 16:03:05 +1100

    All the events

--- game/consumers.py
+++ game/consumers.py
@@ -1,4 +1,3 @@
-import json
 import logging
 
 from channels.generic.websocket import JsonWebsocketConsumer
@@ -7,20 +6,19 @@ from channels.consumer import SyncConsumer
 from .engine import GameEngine
 from .models import Game
 
-from .models import Game
-from .players import Direction, get_player_directions, set_player_direction
+from .players import Direction, set_player_direction
 
 log = logging.getLogger(__name__)
 
 
 class PlayerConsumer(JsonWebsocketConsumer):
     async def connect(self):
-        self.room_name = 'snek'
-        self.room_group_name = '{}_game'.format(self.room_name)
+        self.group_name = 'snek_game'
+        self.game = None
 
         # Join game
         await self.channel_layer.group_add(
-            self.room_group_name,
+            self.group_name,
             self.channel_name
         )
         await self.accept()
@@ -29,33 +27,42 @@ class PlayerConsumer(JsonWebsocketConsumer):
             'game_engine',
             {
                 'type': 'player.new',
-                'id': 'get_email_from_user',
+                'id': 'get_user(whatevs).email',
                 'channel': self.channel_name,
             }
         )
 
     async def disconnect(self):
         # Leave game
+        log.info('Disconnect')
         await self.channel_layer.group_discard(
-            self.room_group_name,
+            self.group_name,
             self.channel_name
         )
 
     # Receive message from Websocket
     async def receive_json(self, content):
-        direction = Direction[content['direction']]
-
-        # update player direction
-        set_player_direction(
-            Game.current_board(),
-            get_user(self.scope),
-            direction
+        log.info('Receive JSON: %s', content)
+        direction = content['direction']
+        await self.channel_layer.send(
+            'game_engine',
+            {
+                'type': 'player.direction',
+                'player': 'me',
+                'direction': direction,
+            }
         )
 
     # Send game data to room group after a Tick is processed
-    async def game_update(self, state):
+    async def game_update(self, event):
+        log.info('Game Update: %s', event)
         # Send message to WebSocket
+        state = event['state']
         await self.send_json(state)
+
+    async def current_game(self, event):
+        game_id = event['game_id']
+        self.game = Game.objects.get(pk=game_id)
 
 
 class GameConsumer(SyncConsumer):
@@ -67,5 +74,16 @@ class GameConsumer(SyncConsumer):
         self.engine = GameEngine(self.game, self.group_name)
         self.engine.start()
 
-    def player_new(self, message):
-        log.info('Player Joined: %s', message)
+    def player_new(self, event):
+        log.info('Player Joined: %s', event)
+        self.engine.player_new()
+
+    def player_direction(self, event):
+        log.info('Player direction changed: %s', event)
+        direction = event.get('direction', 'UP')
+
+        set_player_direction(
+            self.game,
+            event.get('player', 'Player 1'),
+            Direction[direction]
+        )
