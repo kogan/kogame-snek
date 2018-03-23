74cd4f7c6fba579b63c29ef9000450155647916b
Jesse O'Connor <jesse@kogan.com.au>
2018-03-23 12:49:54 +1100

    Use __str__ of game for logging as it includes tick already

--- game/process_game.py
+++ game/process_game.py
@@ -8,23 +8,23 @@ log = logging.getLogger(__name__)
 def process_game_state(game):
 
     # fetch movements from cache
-    log.debug("fetching movements for game: %s, tick: %s", game.pk, game.tick)
+    log.debug("fetching movements for game: %s", game)
     movements = get_movements(game)
 
     # Update all snakes with their directions
-    log.debug("pricessing movements for game: %s, tick: %s", game.pk, game.tick)
+    log.debug("pricessing movements for game: %s", game)
     new_state = process_movements(game, movements)
 
     # Has anyone hit a wall or another snake?
-    log.debug("processing collisions for game: %s, tick: %s", game.pk, game.tick)
+    log.debug("processing collisions for game: %s", game)
     new_state = process_collisions(game, new_state)
 
     # Drop new fruit
-    log.debug("dropping fruit for game: %s, tick: %s", game.pk, game.tick)
+    log.debug("dropping fruit for game: %s", game)
     new_state = add_fruit(game)
 
     # Save new game state
-    log.debug("saving game state for game: %s, tick: %s", game.pk, game.tick)
+    log.debug("saving game state for game: %s", game)
     board = Board.objects.create(state=new_state, game=game)
 
     # successful update
