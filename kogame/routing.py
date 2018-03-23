7bf5efba83487cdf5d934c3f78359314b985226c
Josh Smeaton <josh.smeaton@gmail.com>
2018-03-23 17:00:21 +1100

    Here we go

--- kogame/routing.py
+++ kogame/routing.py
@@ -3,13 +3,10 @@ from channels.sessions import SessionMiddlewareStack
 from django.conf.urls import url
 from game.consumers import GameConsumer, PlayerConsumer
 
-from chat.consumers import ChatConsumer
-
 application = ProtocolTypeRouter({
     "websocket": SessionMiddlewareStack(
         URLRouter([
             url(r'^ws/game/$', PlayerConsumer),
-            url(r'^ws/chat/(?P<room_name>[^/]+)/$', ChatConsumer),
         ]),
     ),
     'channel': ChannelNameRouter({
