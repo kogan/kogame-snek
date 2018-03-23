39176a84ce946145ee1b37c822d38f0d495c9505
Jay Jamero <jay.jamero@kogan.com.au>
2018-03-23 14:26:32 +1100

    WIP: Todo styles

---
+++ src/components/Snek/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './styles.scss'
+
+const Snek = () => (
+  <div className={styles.loader__wrapper}>
+    <span className={styles.snek}></span>
+  </div>
+)
+
+export default Snek
--- src/containers/SnekContainer/index.jsx
+++ src/containers/SnekContainer/index.jsx
@@ -1,26 +1,112 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { COMPONENT_TITLE } from './constants'
+import Snek from 'components/Snek'
+import GridCell from 'components/GridCell'
+import { KEYS } from './constants'
 import styles from './styles.scss'
 
 class SnekContainer extends Component {
-  static propTypes = {};
+  static propTypes = {
+    cellSize: PropTypes.number,
+    board: PropTypes.object,
+    players: PropTypes.object,
+  };
 
-  static defaultProps = {};
+  static defaultProps = {
+    cellSize: 20,
+    board: {},
+    players: {},
+  };
 
   constructor(props) {
     super(props)
     this.state = {
-      players: [],
+      activeDirection: null,
     }
   }
+  setDirection = ({ keyCode }) => {
+    let activeDirection = null
 
-  renderBoard = () => (
-    <div className={styles.boardLayout}>
-      <h2 className="">{COMPONENT_TITLE} Just Another Generic Container</h2>
-    </div>
-  )
+    switch (keyCode) {
+      case KEYS.left:
+        activeDirection = 'UP'
+        break
+      case KEYS.right:
+        activeDirection = 'RIGHT'
+        break
+      case KEYS.up:
+        activeDirection = 'LEFT'
+        break
+      case KEYS.down:
+        activeDirection = 'DOWN'
+        break
+      default:
+        break
+    }
+    this.setState({ activeDirection })
+  };
+
+  renderBoard = () => {
+    const {
+      cellSize,
+      board,
+      players,
+    } = this.props
+
+    const {
+      dimensions,
+      food,
+    } = board
+
+    const cells = []
+    const numRows = dimensions[1]
+    const numCols = dimensions[0]
+    const renderedGridCells = []
+
+    for (let row = 0; row < numRows; row += 1) {
+      const currentRow = []
+      for (let col = 0; col < numCols; col += 1) {
+        currentRow.push({ cellType: null })
+      }
+      cells.push(currentRow)
+    }
+
+    for (let foodPos = 0; foodPos < food.length; foodPos += 1) {
+      const foodX = food[foodPos][0]
+      const foodY = food[foodPos][1]
+      cells[foodX][foodY] = { cellType: 'food' }
+    }
+
+    for (let playerPos = 0; playerPos < players.length; playerPos += 1) {
+      const player = players[playerPos]
+      for (let snakePos = 0; snakePos < player.snake.length; snakePos += 1) {
+        const snakeX = player.snake[snakePos][0]
+        const snakeY = player.snake[snakePos][1]
+        cells[snakeX][snakeY] = { cellType: 'snake' }
+      }
+    }
+
+    for (let row = 0; row < numRows; row += 1) {
+      for (let col = 0; col < numCols; col += 1) {
+        const cellObject = cells[row][col]
+        console.log(cellObject)
+        renderedGridCells.push(<GridCell size={cellSize} cellType={cellObject.cellType} />)
+      }
+    }
+    return (
+      <div
+        tabIndex="1"
+        role="presentation"
+        className={styles.boardLayout}
+        onKeyDown={this.setDirection}
+      >
+        {renderedGridCells}
+        <Snek />
+      </div>
+    )
+  };
 
   render() {
     return this.renderBoard()
