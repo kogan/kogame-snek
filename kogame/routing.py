59f25283cf91afe653a5d479348b474a63d0f467
Jake Barber <jake.b.dev@gmail.com>
2018-03-23 13:47:15 +1100

    added tests for consumers

--- kogame/routing.py
+++ kogame/routing.py
@@ -1,12 +1,12 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.conf.urls import url
-from game.consumers import TestConsumer
+from game.consumers import GameConsumer
 
 from chat.consumers import ChatConsumer
 
 application = ProtocolTypeRouter({
     'websocket': URLRouter([
-        url(r'^test/$', TestConsumer),
+        url(r'^game/$', GameConsumer),
         url(r'^ws/chat/(?P<room_name>[^/]+)/$', ChatConsumer),
     ]),
 })
