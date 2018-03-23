e7ac77e360b4c0a53c0f5ca140cbb6e7b8527b5d
Jay Jamero <jay.jamero@kogan.com.au>
2018-03-23 11:46:20 +1100

    fix eslint issues

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux'
-import genericContainerReducer from '../containers/GenericContainer/reducer'
+import snekReducer from 'containers/SnekContainer/reducer'
 
 // a function that returns a piece of the application state
 // because application can have many different pieces of state == many reducers
 const rootReducer = combineReducers({
-  genericContainerReducer,
+  snekReducer,
 })
 
 export default rootReducer
